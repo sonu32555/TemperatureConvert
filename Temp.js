2ab42f05b447ea7dd799e8649b2180122970eac8
sonu32555 <153379996+sonu32555@users.noreply.github.com>
2023-12-10 17:46:18 +0530

    Add files via upload

---
+++ Temp.js
@@ -0,0 +1,35 @@
+const celsiusField =document.querySelector("#celsius");
+const degree =document.querySelector("#degree");
+const convertBtn =document.querySelector("#convert-btn");
+const tempType=document.querySelector("#temp-type");
+
+window.addEventListener("load", () => {
+    degree.value="";
+    celsiusField.innerHTML = "";
+})
+
+convertBtn.addEventListener("click", (e) =>{
+    e.preventDefault();
+    converttocelcius();
+
+    convertBtn.innerHTML ="<span><i class='fa fa-spinner fa-spin'></i>Converting...</span>";
+    setTimeout(() => {
+        convertBtn.innerHTML="<span>Convert</span>";   
+    },1000)
+})
+
+function converttocelcius() {
+    let inputvalue = degree.value;
+
+   setTimeout(() =>{
+    if(tempType.value === "fahrenheit"){
+        const fahrenheitToCelsius =(inputvalue - 32) * (5/9);
+        celsiusField.innerHTML =`${fahrenheitToCelsius.toFixed(3)} &deg;c`;
+    }
+    else if(tempType.value === "kelvin"){
+        const kelvinToCelsius = (inputvalue - 273.15);
+        celsiusField.innerHTML = `${kelvinToCelsius.toFixed(3)} &deg;c`; 
+        
+    }
+   },1200 )
+}
